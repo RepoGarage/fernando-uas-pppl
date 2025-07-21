8fd5b68c971d76e2c0e0c0ace2be91504fb87cbd
Fernando Perry <fernandoperry1234@gmail.com>
2025-07-21 19:37:06 +0700

    add typing effect

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -7,10 +7,39 @@ export function render_home() {
                 <img src="./static/hero.jpg" class="w-full h-full object-cover opacity-80"/>
                 <div class="relative bottom-32 left-0 w-full h-32 bg-gradient-to-t from-[#1e1e1e]/100 to-transparent"></div>
         </div>
-        <h1 class="text-red-300 text-5xl ml-7 mr-7 font-bold">Smart Living Starts Here</h1>
+        <h1 id="animated-tagline" class="text-red-300 text-5xl ml-7 mr-7 font-bold p-1">&nbsp</h1>
+        <!-- <h1 id="animated-tagline" class="text-red-300 text-5xl ml-7 mr-7 font-bold">Smart Living Starts Here</h1> -->
         <div id="yeah-boi" class="flex m-3 ml-7 font-mono"></div>
+        <p class="ml-16 mr-16">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <p class="ml-16 mr-16">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <p class="ml-16 mr-16">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <p class="ml-16 mr-16">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <p class="ml-16 mr-16">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
         `;
     }
+    const taglineElement = document.getElementById("animated-tagline");
+    const taglineText = "Smart Living Starts Here";
+    let i = 0;
+    let typingSpeed = 75; // Adjust typing speed (milliseconds per character)
+
+    async function typeWriter() {
+        if (taglineElement && taglineElement.innerText.length >= taglineText.length)
+            taglineElement.textContent = taglineElement.innerText.slice(0, taglineElement.innerText.length - 1);
+        if (i < taglineText.length) {
+            if (i == 0 && taglineElement) taglineElement.textContent = "";
+            if (taglineElement) {
+                if (taglineElement && taglineElement.textContent?.endsWith('|'))
+                    taglineElement.textContent = taglineElement.textContent.slice(0, taglineElement.textContent.length - 1);
+                taglineElement.innerHTML += taglineText.charAt(i);
+                taglineElement.innerHTML += '|';
+            }
+            i++;
+            setTimeout(typeWriter, typingSpeed);
+        }
+    }
+
+    setTimeout(typeWriter, 500);
+
     const yb = document.getElementById("yeah-boi");
     if (yb) {
         yb.appendChild(create_default_button("Learn More", () => {console.log("clicked");}).obj);
